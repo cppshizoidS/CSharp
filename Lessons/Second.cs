b5d8bcc5ee07d7d4f83545512dcfeb41c9ab808a
Cppshizoid <alex.krivoschecov@gmail.com>
2022-04-04 00:23:13 +0300

    Create Second.cs

---
+++ Lessons/Second.cs
@@ -0,0 +1,15 @@
+/*basics math operations*/
+int x = 10;
+int y = 5;
+int z = x + y;
+int a = x - y;
+double s = 0;
+
+string t = Console.ReadLine();
+int u = Convert.ToInt32(t);
+
+
+Console.WriteLine(z);
+Console.WriteLine(a);
+Console.WriteLine(t);
+Console.WriteLine(u);
